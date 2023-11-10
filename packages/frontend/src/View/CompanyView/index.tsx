aa14681b9b19e35d93669b45b6eed24d4099d81c
patrik200 <dalbab@inbox.ru>
2023-11-11 02:14:08 +0300

    sdsd

--- packages/frontend/src/View/CompanyView/index.tsx
+++ packages/frontend/src/View/CompanyView/index.tsx
@@ -1,19 +1,18 @@
 import Card from "components/Card";
 import TitleCard from "components/Card/TitleCard"
+
 import { useStore } from "context"
 import { observer } from "mobx-react-lite"
 import { useMemo } from "react";
-import Actions from "./Actions";
 
 const CompanyView = () => {
+    
     const { companyStore } = useStore()
 
     useMemo(() => companyStore.loadCompany(), [companyStore])
 
-    console.log(companyStore.entity)
-
     return <>
-        <TitleCard subtitle="Ваша компания" actions={<Actions />}>{companyStore.entity!.name}</TitleCard>
+        <TitleCard subtitle="Ваша компания">{companyStore.entity!.name}</TitleCard>
         <Card title="Описание">{companyStore.entity?.description}</Card>
     </>
 }
