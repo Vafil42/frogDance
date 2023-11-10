657be3219eb9f826728743fa5e9e9cf56d25ef9f
a-aniskin <a.aniksin2006@gmail.com>
2023-11-10 19:41:57 +0300

    Company model added

--- packages/backend/app/__init__.py
+++ packages/backend/app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from packages.backend.app.config import Config
 from packages.backend.app.database import init_db
-from packages.backend.app.routes.controller import init_controller, route_blueprint
+from packages.backend.app.routes.controller import init_controller as route_controller, route_blueprint
 from packages.backend.app.routes.service import RouteService
+from packages.backend.app.companies.controller import init_controller as company_controller, company_blueprint
+from packages.backend.app.companies.service import CompanyService
 
 
 def create_app():
@@ -11,7 +13,11 @@ def create_app():
     db = init_db(app)
 
     routeService = RouteService(db)
-    init_controller(routeService)
+    route_controller(routeService)
     app.register_blueprint(route_blueprint)
+
+    companyService = CompanyService(db)
+    company_controller(companyService)
+    app.register_blueprint(company_blueprint)
 
     return app
--- packages/backend/run.py
+++ packages/backend/run.py
@@ -1,4 +1,5 @@
 from app import create_app
+from flask import request
 
 
 app = create_app()
@@ -8,6 +9,11 @@ app = create_app()
 @app.route("/index")
 def hello():
     return "Hello, world!"
+
+
+@app.route("/get", methods=["POST"])
+def get():
+    return request.json
 
 
 if __name__ == "__main__":
