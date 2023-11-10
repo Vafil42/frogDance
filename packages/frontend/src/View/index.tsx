9f169661ed7cb005bb656263c66eb63387244d7a
patrik200 <dalbab@inbox.ru>
2023-11-10 13:49:34 +0300

    sd

--- packages/frontend/src/View/index.tsx
+++ packages/frontend/src/View/index.tsx
@@ -1,11 +1,32 @@
 import { useMemo } from "react"
 import { RootStore } from "context"
-import { Outlet } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom"
+import { headerStyles, linkStyles, linksStyles, titleStyles } from "./style.css"
 
 const AppView = () => {
     const store = useMemo(() => new RootStore, [])
+    const headerRoutes = [
+        {
+            path: "main",
+            placeholder: "Главная"
+        },
+        {
+            path: "company",
+            placeholder: "Компания"
+        },
+        {
+            path: "flow",
+            placeholder: "Маршруты"
+        }
+    ]
 
     return <div>
+        <div className={headerStyles}>
+            <div className={titleStyles}>Frog Dance</div>
+            <div className={linksStyles}>
+                {headerRoutes.map((route, index) => <Link className={linkStyles} key={index} to={route.path}>{route.placeholder}</Link>)}
+            </div>
+        </div>
         <Outlet context={store} />
     </div>
 }
