e4b6df47285702919339c806919adf0afb98d8b4
a-aniskin <a.aniksin2006@gmail.com>
2023-11-10 19:42:01 +0300

    Company model added

---
+++ packages/backend/app/companies/controller.py
@@ -0,0 +1,39 @@
+from flask import Blueprint, request
+
+
+def init_controller(service):
+    global companyService
+    companyService = service
+
+
+company_blueprint = Blueprint("company_api", __name__)
+
+
+@company_blueprint.route("/companies/get-one/<string:id>", methods=["GET"])
+async def getOne(id):
+    return await companyService.getOne(id)
+
+
+@company_blueprint.route("/companies/get-many", methods=["GET"])
+async def getMany():
+    return await companyService.getMany()
+
+
+@company_blueprint.route("/companies/get-one", methods=["POST"])
+async def create():
+    return await companyService.create(request.json)
+
+
+@company_blueprint.route("/companies/get-one", methods=["POST"])
+async def logIn():
+    return await companyService.logIn(request.json)
+
+
+@company_blueprint.route("/companies/get-one/<string:id>", methods=["PUT"])
+async def update(id):
+    return await companyService.update(id, request.json)
+
+
+@company_blueprint.route("/companies/get-one/<string:id>", methods=["DELETE"])
+async def delete(id):
+    return await companyService.delete(id)
